4e648703be1068e61d053308ce1763247684d9ef
Veena Chandran <vchandran2@ymail.com>
2017-05-23 15:33:01 -0700

    prepare, acknowledge, and sending accepts are almost done

--- paxos.py
+++ paxos.py
@@ -5,23 +5,61 @@ import time
 
 class Paxos():
     def __init__(self, index, ID):
-        self.numVotes = 0
+        self.rcvdVotes = {} # upon receiving ack, add (channel:acknowledge msg) to this dict. not including mine
         self.numAccepts = 0
-        self.index = index
+        self.index = index # for when we expand to PRM
         self.ballotNum = [0, 0]
         self.acceptNum = [0, 0]
         self.outgoingTCP = {}
         self.incomingTCP = {}
-        self.val = None
+        self.val = None # null until a value has been accepted by majority
         self.ID = ID
+        self.sites = {} # all the sites and their TCPs
+        self.proposedVal = None # when a process wants to propose a value, it'll be stored here
 
     def rcvPrepare(self, data, channel):
-        ballotRcvd = data[1]
+        ballotRcvd = list(map(int, data[1].strip('[]').split(',')))
+        print(ballotRcvd)
         if ((ballotRcvd[0] > self.ballotNum[0])
             | (ballotRcvd[0] == self.ballotNum[0])
             & (ballotRcvd[1] > self.ballotNum[1])):
             self.ballotNum = ballotRcvd
-            print(channel)
+            self.outgoingTCP.get(channel).sendall(str('ack|'
+                                                  + str(self.ballotNum) + '|'
+                                                  + str(self.acceptNum) + '|'
+                                                  + str(self.val)
+                                                  ).encode())
+            print('sent ack ' + str(self.ballotNum) + ' to ' + str(channel))
+
+    def rcvAck(self, data, channel):
+        majority = (len(self.sites) // 2) + 1
+        self.rcvdVotes[channel] = data
+        # if the number of rcvdVotes plus mine is a majority
+        if (len(self.rcvdVotes) + 1 >= majority):
+            # if all received values are None, then we set my val to my proposedVal
+            # if not, then we set my val to the val that we received with the highest ballot
+            maxVote = None
+            for vote in self.rcvdVotes:
+                if (self.rcvdVotes[vote][3] != 'None'):
+                    ballotRcvd = list(map(int, self.rcvdVotes[vote][1].strip('[]').split(',')))
+                    if (maxVote == None):
+                        maxVote = self.rcvdVotes[vote]
+
+                    elif ((ballotRcvd[0] > list(map(int, maxVote[1].strip('[]').split(',')))[0])
+                            | (ballotRcvd[0] == list(map(int, maxVote[1].strip('[]').split(',')))[0])
+                            & (ballotRcvd[1] > list(map(int, maxVote[1].strip('[]').split(',')))[1])):
+                        maxVote = self.rcvdVotes[vote]
+            print('maxVote = ' + str(maxVote))
+            if (maxVote == None):
+                self.val = self.proposedVal
+            else:
+                self.val = maxVote[3]
+        for out in self.outgoingTCP:
+            self.outgoingTCP.get(out).sendall(str('accept|'
+                                                  + str(self.ballotNum) +'|'
+                                                  + str(self.val)
+                                                  ).encode())
+            print('sent accept ' + str(self.val) + ' to ' + str(out))
 
     def receiveMsgs(self, incomingTCP):
         while True:
@@ -29,19 +67,22 @@ class Paxos():
                 try:
                     data = incomingTCP.get(channel).recv(1024)
                     data = data.decode().strip().split('|')
+                    # prepare msg looks like this: prepare|ballotNum
                     if (data[0] == 'prepare'):
                         self.rcvPrepare(data, channel)
+                    # ack msg looks like this: ack|ballotNum|acceptNum|val
+                    if (data[0] == 'ack'):
+                        self.rcvAck(data, channel)
                 except socket.error:
                     break
 
     def setup(self):
         f = open('setup.txt', 'r')
         numProc = int(f.readline().strip())
-        sites = {}
         for i in range(numProc):
-            sites[i + 1] = f.readline().strip().split()
-        print(sites)
-        TCP = sites.get(self.ID)
+            self.sites[i + 1] = f.readline().strip().split()
+        print(self.sites)
+        TCP = self.sites.get(self.ID)
         print(TCP)
         TCP_IP = TCP[0]
         TCP_PORT = int(TCP[1])
@@ -58,7 +99,7 @@ class Paxos():
             if (self.ID == sender):
                 while True:
                     try:
-                        TCP = sites.get(int(recvr))
+                        TCP = self.sites.get(int(recvr))
                         TCP_IP = TCP[0]
                         TCP_PORT = int(TCP[1])
                         n = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -77,12 +118,13 @@ class Paxos():
         if (self.ID == 1):
             self.ballotNum[0] = 1
             self.ballotNum[1] = self.ID
+            self.proposedVal = 5
             for out in self.outgoingTCP:
-                self.outgoingTCP.get(out).sendall(str('MARKER'
-                                                                 + '-'
-                                                                 + str(out)
-                                                                 + '-2|').encode())
+                self.outgoingTCP.get(out).sendall(str('prepare|'
+                                                                 + str(self.ballotNum)
+                                                                 ).encode())
                 print('sent to ' + str(out))
+            self.receiveMsgs(self.incomingTCP)
         else:
             self.receiveMsgs(self.incomingTCP)
 
