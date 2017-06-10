b495af770c34ac9e72b33aa3b430b3adfc0c8b22
Veena Chandran <vchandran2@ymail.com>
2017-06-09 17:08:28 -0700

    mainreducer

---
+++ mainReducer.py
@@ -0,0 +1,11 @@
+from reducer import Reducer
+import sys
+
+def main():
+    trial = Reducer(1,2,3)
+    trial.reduce('littlefile2.txt littlefile_I_3.txt')
+
+
+
+if __name__ == "__main__":
+    main()
